af6fb2232c7f5e2a2bc1ddc98dc9914e3a9ae6a6
MacaronFR <denisft77@gmail.com>
2022-06-08 15:29:47 +0200

    error embed

--- src/main/kotlin/fr/motrelou/bot/commands/Add.kt
+++ src/main/kotlin/fr/motrelou/bot/commands/Add.kt
@@ -1,11 +1,10 @@
 package fr.motrelou.bot.commands
 
 import dev.kord.common.Color
-import dev.kord.common.entity.Snowflake
 import dev.kord.core.Kord
 import dev.kord.core.behavior.interaction.respondPublic
-import dev.kord.rest.builder.message.create.embed
 import fr.motrelou.bot.api.API
+import fr.motrelou.bot.embeds.embedErreur
 import fr.motrelou.bot.embeds.embedMot
 import fr.motrelou.bot.excpetions.APIException
 import fr.motrelou.bot.getUserInGuild
@@ -18,6 +17,7 @@ class Add(kord: Kord, private val api: API) : RegisterCommand(kord, Command(
 		Parameter(ParameterType.String, "definition", "Définition du mot", true)
 	)
 ), {
+	val user = kord.getUserInGuild(interaction.user.id, interaction.guildId)!!
 	try {
 		val result = api.motAjout(
 			interaction.command.strings["mot"]!!,
@@ -25,23 +25,13 @@ class Add(kord: Kord, private val api: API) : RegisterCommand(kord, Command(
 			interaction.user.id.toString()
 		)
 		interaction.respondPublic {
-			embedMot(result, kord.getUserInGuild(Snowflake(result.createur), interaction.guildId)!!)
+			embedMot(result, user)
 		}
 	}catch(e: APIException){
 		interaction.respondPublic {
-			embed {
-				author {
-					name = "Bot Mot Relou"
-				}
-				title = "Erreur"
-				description = when(e.code){
-					409 -> "Mot existant"
-					else -> "Erreur Serveur"
-				}
-				color = when(e.code){
-					in 400..499 -> Color(255, 127, 0)
-					else -> Color(255, 0, 0)
-				}
+			when(e.code){
+				409 -> embedErreur("Mot existant", user, "Le mot **${interaction.command.strings["mot"]!!}** existe déjà dans la base", Color(255, 127, 0))
+				else -> embedErreur("Critique", user, "Erreur serveur", Color(255, 0, 0))
 			}
 		}
 	}
---
+++ src/main/kotlin/fr/motrelou/bot/embeds/ErreurEmbed.kt
@@ -0,0 +1,23 @@
+package fr.motrelou.bot.embeds
+
+import dev.kord.common.Color
+import dev.kord.core.entity.Member
+import dev.kord.rest.builder.message.create.MessageCreateBuilder
+import dev.kord.rest.builder.message.create.embed
+import kotlinx.datetime.Clock
+
+fun MessageCreateBuilder.embedErreur(message: String, user: Member, description: String, color: Color){
+	embed {
+		author {
+			name = user.displayName
+			icon = user.avatar?.url
+		}
+		title = "Erreur : $message"
+		this.description = description
+		this.color = color
+		timestamp = Clock.System.now()
+		footer {
+			text = "Bot mot relou"
+		}
+	}
+}
