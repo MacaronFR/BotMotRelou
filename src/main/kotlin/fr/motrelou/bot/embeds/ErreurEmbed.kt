5972833f39d2ab9f71e79433de9a256f0d221784
MacaronFR <denisft77@gmail.com>
2022-06-10 16:42:34 +0200

    better embed builder

--- src/main/kotlin/fr/motrelou/bot/embeds/ErreurEmbed.kt
+++ src/main/kotlin/fr/motrelou/bot/embeds/ErreurEmbed.kt
@@ -2,44 +2,39 @@ package fr.motrelou.bot.embeds
 
 import dev.kord.common.Color
 import dev.kord.core.entity.Member
+import dev.kord.rest.builder.message.EmbedBuilder
 import dev.kord.rest.builder.message.create.MessageCreateBuilder
 import dev.kord.rest.builder.message.create.embed
-import dev.kord.rest.builder.message.modify.InteractionResponseModifyBuilder
+import dev.kord.rest.builder.message.modify.MessageModifyBuilder
 import dev.kord.rest.builder.message.modify.embed
 import fr.motrelou.bot.error
 import kotlinx.datetime.Clock
 
 fun MessageCreateBuilder.embedErreur(message: String, user: Member, description: String, color: Color) {
 	embed {
-		author {
-			name = user.displayName
-			icon = user.avatar?.url
-		}
-		title = "Erreur : $message"
-		this.description = description
-		this.color = color
-		timestamp = Clock.System.now()
-		footer {
-			text = "Bot mot relou"
-		}
+		buildErreur(message, user, description, color)
 	}
 }
 
-fun InteractionResponseModifyBuilder.embedErreur(message: String, user: Member, description: String, color: Color) {
+fun MessageModifyBuilder.embedErreur(message: String, user: Member, description: String, color: Color) {
 	embed {
-		author {
-			name = user.displayName
-			icon = user.avatar?.url
-		}
-		title = "Erreur : $message"
-		this.description = description
-		this.color = color
-		timestamp = Clock.System.now()
-		footer {
-			text = "Bot mot relou"
-		}
+		buildErreur(message, user, description, color)
+	}
+}
+
+private fun EmbedBuilder.buildErreur(message: String, user: Member, description: String, color: Color){
+	author {
+		name = user.displayName
+		icon = user.avatar?.url
+	}
+	title = "Erreur : $message"
+	this.description = description
+	this.color = color
+	timestamp = Clock.System.now()
+	footer {
+		text = "Bot mot relou"
 	}
 }
 
 fun MessageCreateBuilder.embedServerError(user: Member) = embedErreur("Critique", user, "Erreur serveur", Color.error)
-fun InteractionResponseModifyBuilder.embedServerError(user: Member) = embedErreur("Critique", user, "Erreur serveur", Color.error)
+fun MessageModifyBuilder.embedServerError(user: Member) = embedErreur("Critique", user, "Erreur serveur", Color.error)
